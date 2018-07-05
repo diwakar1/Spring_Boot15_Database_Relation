f859a4edad07fb3d430fbed59e9f2c2d22ad53ca
nosukja99 <diwakar1@umbc.edu>
2018-07-05 13:17:56 -0400

    first commit

---
+++ src/main/java/com/example/lesson15/HomeController.java
@@ -0,0 +1,39 @@
+package com.example.lesson15;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @RequestMapping("/")
+    public String index (Model model){
+        Employee employee = new Employee();
+        employee.setName("Partik kanchha");
+        employee.setSsn("777-666-6789");
+
+
+        Laptop laptop =new Laptop();
+        laptop.setBrand("Dell");
+        laptop.setModel("Latitude");
+
+
+
+        employee.setLaptop(laptop);
+
+        employeeRepository.save(employee);
+
+
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "index";
+
+
+
+    }
+
+}
